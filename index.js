8ffdebb53e313bfd5c811695d3b0f33b4bf2ab99
Alexandra Pestruyeva <apestruy@gmail.com>
2019-10-22 03:26:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 function dwarfRollCall(dwarves) {
-}
+  var array = []
+  for (var i = 0; i < dwarves.length; i++) {
+    array.push(`${i + 1}. ${dwarves[i]} `) }
+    return array.join("") 
+} 
 
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
 function summonCaptainPlanet(planeteerCalls){
+  var array = []
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    array.push(planeteerCalls[i].toUpperCase() + "!") }
+  return array 
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {return true }
+    else { return false } 
+  }
 }
 
 function findTheCheese (foods) {
-}
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar"
+    || foods[i] === "gouda"
+    || foods[i] === "camembert") 
+     { return foods[i] } }
+     return "no cheese!" } 
