365e91c668e5bd4a835a0e5fb4ae6a3f6382f9d4
Alexandra Pestruyeva <apestruy@gmail.com>
2020-02-02 07:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 function dwarfRollCall(dwarves) {
   var array = []
-  for (var i = 0; i < dwarves.length; i++) {
+  for (var i = 0; i < dwarves.length / 2; i++) {
     array.push(`${i + 1}. ${dwarves[i]} `) }
     return array.join("") 
 } 
@@ -25,6 +25,15 @@ function findTheCheese (foods) {
   for (var i = 0; i < foods.length; i++) {
     if (foods[i] === "cheddar"
     || foods[i] === "gouda"
-    || foods[i] === "camembert") 
+    || foods[i] === "camembert"
+    || foods[i] === "swiss") 
      { return foods[i] } }
      return "no cheese!" } 
+     
+function wordsWithB (words) {
+  var array = []
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].startsWith("b")) {
+    array.push(words[i]) } }
+   return array
+  } 
